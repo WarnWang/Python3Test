0d82c9f9e5003be20d043831d2fe56f3f72a1256
Mark Wang <wangyouan0629@hotmail.com>
2017-09-28 18:02:21 +0800

    fail to download all papers

--- download_qje_papers/step3_download_all_papers.py
+++ download_qje_papers/step3_download_all_papers.py
@@ -10,6 +10,7 @@ import os
 import requests
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 from download_qje_papers.utilities import switch_to_non_default_handle
 
@@ -18,6 +19,7 @@ driver = webdriver.Chrome('/Users/warn/PycharmProjects/Python3Test/download_qje_
 lib_url = 'http://lib.hku.hk/'
 
 login_button = '//*[@id="body_container"]/div[2]/div[1]/div/a'
+submit_button = '//*[@id="main-content"]/form/div/div[3]/div[3]/button[1]'
 
 driver.get(lib_url)
 
@@ -26,4 +28,8 @@ switch_to_non_default_handle(driver)
 
 driver.find_element_by_name('userid').send_keys('markwang')
 driver.find_element_by_name('password').send_keys('az1212AZ1212')
+driver.find_element_by_xpath(submit_button).click()
 
+driver.find_element_by_id('search_field').clear()
+driver.find_element_by_id('search_field').send_keys('Outside and Inside Liquidity')
+driver.find_element_by_id('search_field').send_keys(Keys.ENTER)
