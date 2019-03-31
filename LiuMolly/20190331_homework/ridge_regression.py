fb443f5721565f2f3f23b7b6d72209d20a69bb03
Mark Wang <wangyouan0629@hotmail.com>
2019-03-31 12:21:56 +0800

    finish ridge

---
+++ LiuMolly/20190331_homework/ridge_regression.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Filename: ridge_regression
+# @Date: 2019-03-31
+# @Author: Mark Wang
+# @Email: wangyouan@gamil.com
+
+import os
+from datetime import datetime
+
+import pandas as pd
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from pandas.plotting import register_matplotlib_converters
+
+# python machine learning package, for linear model, you can also use linearmodels to add fixed effects
+from sklearn.linear_model import Ridge
+
+# Define variables
+STOCK_SYMBOL = 'SYMBOL'
+DATE = 'DATE'
+RETURN = 'PRICE_CHG'
+RETURN_1 = 'returns_1'
+PREDICT_RETURN_1 = 'predict_return_1'
+PREDICT_RANK = 'predict_rank'
+REAL_RANK = 'real_rank'
+
+# some parameters hear
+window_size = 4
+
+# variable name that will be used in regression you can choose which variables used for x
+x_vars = ['VOLUME', 'AMT', 'PRICE_CHG', 'PCT_RET', 'AVG_PRICE', 'TURNOVER', 'MKT_CAP', 'ISSUED_SHARE', 'PB(HPS)',
+          'PCF', 'PB', 'PE_T12MOB', 'PSALES', 'EPSY_FY0', 'TRL_YLD', 'RTN12_1M', 'RTN252D',
+          'MA_CO_15_36W', 'REAL_VOL_1YD', 'SKEW_1YD', 'CFY_IS', 'CFROC_CF', 'SHORT_COV',
+          'MOM1M', 'MOM12M', 'INDMOM', 'MAXRET', 'CHMOM', 'RETVOL', 'DOLVOL', 'MOM6M', 'SP', 'TURN', 'STD_TURN',
+          'ILL', 'EP', 'MOM36M', 'ZEROTRADE', 'BM', 'BM_IA']
+y_var = RETURN_1
+
+tqdm.pandas()
+register_matplotlib_converters()
+
+
+# generate a shift to return, used for train and prediction
+def get_next_return(df):
+    df[RETURN_1] = df[RETURN].shift(-1)
+    return df
+
+
+if __name__ == '__main__':
+    plt.style.use('seaborn-white')
+
+    filename = os.path.join('/Users/warn/Google Drive/LiuMolly/20190331_homework',
+                            '20190313 82 variables 300 stocks.csv')
+    data_df = pd.read_csv(filename)
+
+    data_df_add_next_return = data_df.groupby(STOCK_SYMBOL).progress_apply(get_next_return).reset_index(drop=True)
+
+    # this are useful numeric variables
+    numeric_vars = x_vars[:]
+    numeric_vars.append(y_var)
+
+    useful_vars = numeric_vars[:]
+    useful_vars.extend([STOCK_SYMBOL, DATE])
+
+    # only keep useful data
+    data_df_useful = data_df_add_next_return[useful_vars].copy()
+
+    # change data type
+    data_df_useful.loc[:, DATE] = pd.to_datetime(data_df_useful[DATE])
+    for key in numeric_vars:
+        data_df_useful.loc[:, key] = pd.to_numeric(data_df_useful[key], errors='coerce')
+
+    # split data to prepare for train and test
+    data_df_useful = data_df_useful.dropna(how='any')
+    date_list = data_df_useful[DATE].drop_duplicates().sort_values().to_list()
+    test_date_list = date_list[-25:]
+
+    predict_result = pd.DataFrame(columns=[STOCK_SYMBOL])
+    real_result = pd.DataFrame(columns=[STOCK_SYMBOL])
+
+    for date_index in tqdm(range(25, 1, -1)):
+        train_date_list = date_list[-(date_index + window_size):-date_index]
+        test_date: datetime = date_list[-date_index]
+        target_date: datetime = date_list[-(date_index - 1)]
+
+        train_data_df = data_df_useful.loc[data_df_useful[DATE].isin(set(train_date_list))].copy()
+        test_data_df = data_df_useful.loc[data_df_useful[DATE] == test_date].copy()
+
+        # training model and use the model to predict return
+        reg = Ridge(alpha=8, normalize=True).fit(train_data_df[x_vars], train_data_df[y_var])
+        test_data_df.loc[:, PREDICT_RETURN_1] = reg.predict(test_data_df[x_vars])
+        test_data_df.loc[:, PREDICT_RANK] = test_data_df[PREDICT_RETURN_1].rank(ascending=False)
+        test_data_df.loc[:, REAL_RANK] = test_data_df[RETURN_1].rank(ascending=False)
+        predict_result: pd.DataFrame = predict_result.merge(
+            test_data_df[[STOCK_SYMBOL, PREDICT_RANK]].rename(columns={PREDICT_RANK: target_date.strftime('%Y%m%d')}),
+            on=[STOCK_SYMBOL], how='outer')
+        real_result: pd.DataFrame = real_result.merge(
+            test_data_df[[STOCK_SYMBOL, REAL_RANK]].rename(columns={REAL_RANK: target_date.strftime('%Y%m%d')}),
+            on=[STOCK_SYMBOL], how='outer')
+
+    corr = real_result.corrwith(predict_result)
+    print('mean correlation =', corr.mean())
+    # plt.plot(test_date_list[1:], corr, 'b-.')
+
+    # plt.xticks(rotation=90)
+    # plt.show()
--- LiuMolly/20190331_homework/sample_code.py
+++ LiuMolly/20190331_homework/sample_code.py
@@ -6,76 +6,16 @@
 # @Author: Mark Wang
 # @Email: wangyouan@gamil.com
 
+import os
+import sys
+import warnings
 
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import axes3d
-import seaborn as sns
-
-from sklearn.preprocessing import scale
-
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
-
-import sklearn.linear_model as skl_lm
-from sklearn.metrics import mean_squared_error, r2_score
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
-
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.externals.six import StringIO
-from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier, export_graphviz
-from sklearn.ensemble import BaggingClassifier, RandomForestClassifier, BaggingRegressor, RandomForestRegressor, \
-    GradientBoostingRegressor
-from sklearn.metrics import mean_squared_error, confusion_matrix, classification_report
-from sklearn.linear_model import LinearRegression, Ridge, RidgeCV, Lasso, LassoCV
-from sklearn.linear_model import LinearRegression, Ridge, RidgeCV, Lasso, LassoCV
-from sklearn.ensemble import AdaBoostRegressor
-
+import pandas as pd
 import xgboost as xgb
-
-plt.style.use('seaborn-white')
-
-filename = '20190313 82 variables 300 stocks.csv'
-df = pd.read_csv(filename)
-
-df = df.dropna(subset=['SYMBOL', 'PCT_RET', 'VOLUME'])
-
-df['returns'] = df['PCT_RET']
-df['Date2'] = pd.to_datetime(df.DATE)
-df['Stock'] = df['SYMBOL']
-
-period = pd.unique(df.Date2)  # this is all the tiem periods in the dataset
-period = np.sort(period)
-stocks = pd.unique(df.SYMBOL)  # this is all the stock in the dataset
-# stocks=np.delete(stocks,4,axis=0)
-
-genindustrydummy = 1  # generate industry dummies
-if genindustrydummy == 1:
-    df = pd.get_dummies(df, columns=['SIC2'])
-    allvariables = list(df)
-    industryvariable = allvariables[-17:]
-
-#######################################################################
-# Check missing data (some periods may have few stocks (less than 100))#
-#######################################################################
-
-T0 = period.size
-stock_number_each_period = []
-for i in range(T0):
-    data2 = df.loc[(df['Date2'] == period[i]), ['returns']]
-    stock_number_each_period.append(len(data2) - data2.returns.isna().sum())
-
-stock_number_each_period2 = pd.DataFrame(data=stock_number_each_period)
-stock_number_each_period2['period'] = period
-stock_number_each_period2.columns = ['number of stocks', 'period']
-validdate = stock_number_each_period2.loc[(stock_number_each_period2['number of stocks'] > 100), 'period']
-validdate_value = validdate.values
-
-T = validdate_value.size
-Stock300 = pd.DataFrame(data=stocks)
-Stock300.columns = ['Stock']
+from statistics import mean
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 
 ###############################################################################
@@ -110,7 +50,7 @@ def yxdata(t, validdate, df, ranky, rankx):
     return yx_data
 
 
-def showcorrs(pred_y, test_y):
+def show_corrs(pred_y, test_y):
     comdf = pd.DataFrame({'pred_y': pred_y[:, 0], 'test_y': test_y[:, 0]})
     comdf['pred_y_rank'] = comdf['pred_y'].rank(ascending=0)
     comdf['test_y_rank'] = comdf['test_y'].rank(ascending=0)
@@ -119,66 +59,107 @@ def showcorrs(pred_y, test_y):
     return rank_corr, return_corr
 
 
-###############################################################################
-##########################Foecasting ##########################################
-###############################################################################
+if __name__ == '__main__':
+    plt.style.use('seaborn-white')
 
+    filename = os.path.join('/Users/warn/Google Drive/LiuMolly/20190331_homework',
+                            '20190313 82 variables 300 stocks.csv')
+    df = pd.read_csv(filename)
 
-x_variable_selection = ['MOM12M', 'CHMOM', 'MAXRET', 'RETVOL', 'DOLVOL', 'SP', 'STD_TURN', 'MOM6M', 'MKT_CAP', 'MOM1M',
-                        'MOM36M', 'DY']
+    df = df.dropna(subset=['SYMBOL', 'PCT_RET', 'VOLUME'])
 
-x_variable_names = x_variable_selection[:]
-if genindustrydummy == 1:
-    x_variable_names.extend(industryvariable)
+    df['returns'] = df['PCT_RET']
+    df['Date2'] = pd.to_datetime(df.DATE)
+    df['Stock'] = df['SYMBOL']
 
-import sys
-import warnings
+    period = pd.unique(df.Date2)  # this is all the tiem periods in the dataset
+    period = np.sort(period)
+    stocks = pd.unique(df.SYMBOL)  # this is all the stock in the dataset
+    # stocks=np.delete(stocks,4,axis=0)
 
-if not sys.warnoptions:
-    warnings.simplefilter("ignore")
+    gen_industry_dummy = 1  # generate industry dummies
+    if gen_industry_dummy == 1:
+        df = pd.get_dummies(df, columns=['SIC2'])
+        all_variables = list(df)
+        industry_variables = all_variables[-17:]
+    else:
+        industry_variables = []
 
-ranky = 0  # whether you want to transform y variable to ranks
-rankx = 0  # whether you want to transform x variables to ranks
-standardized = 1  # whether you want to standardize data
+    #######################################################################
+    # Check missing data (some periods may have few stocks (less than 100))#
+    #######################################################################
 
-test_period = 24  # the number of peridos for testing
-lag_period = 3  # the number of periods used in the training
+    T0 = period.size
+    stock_number_each_period = []
+    for i in range(T0):
+        data2 = df.loc[(df['Date2'] == period[i]), ['returns']]
+        stock_number_each_period.append(len(data2) - data2.returns.isna().sum())
 
-saved_corr = []
+    stock_number_each_period2 = pd.DataFrame(data=stock_number_each_period)
+    stock_number_each_period2['period'] = period
+    stock_number_each_period2.columns = ['number of stocks', 'period']
+    validdate = stock_number_each_period2.loc[(stock_number_each_period2['number of stocks'] > 100), 'period']
+    validdate_value = validdate.values
 
-for t in range(T - test_period, T):
-    traindata = yxdata(t - 1, validdate, df, ranky, rankx)
-    for lagt in range(2, lag_period):
-        temptraindata = yxdata(t - lagt, validdate, df, ranky, rankx)
-        traindata = pd.concat([traindata, temptraindata])
+    T = validdate_value.size
+    Stock300 = pd.DataFrame(data=stocks)
+    Stock300.columns = ['Stock']
 
-    testdata = yxdata(t, validdate, df, ranky, rankx)
+    ###############################################################################
+    ##########################Foecasting ##########################################
+    ###############################################################################
 
-    train_y = traindata['yreturns'].values.reshape(-1, 1)
-    test_y = testdata['yreturns'].values.reshape(-1, 1)
+    x_variable_selection = ['MOM12M', 'CHMOM', 'MAXRET', 'RETVOL', 'DOLVOL', 'SP', 'STD_TURN', 'MOM6M', 'MKT_CAP',
+                            'MOM1M',
+                            'MOM36M', 'DY']
 
-    train_x = traindata[x_variable_names].values
-    test_x = testdata[x_variable_names].values
+    x_variable_names = x_variable_selection[:]
+    if gen_industry_dummy == 1:
+        x_variable_names.extend(industry_variables)
 
-    if standardized == 1:
-        scaler = StandardScaler().fit(train_x)
-        train_x = scaler.transform(train_x)
-        test_x = scaler.transform(test_x)
+    if not sys.warnoptions:
+        warnings.simplefilter("ignore")
 
-    method = xgb.XGBRegressor(objective='reg:linear', colsample_bytree=0.25, learning_rate=0.002, max_depth=5, alpha=50,
-                              n_estimators=5000, random_state=0)
-    method.fit(train_x, train_y)
-    pred_y = method.predict(test_x)
-    pred_y = pred_y.reshape(-1, 1)
+    ranky = 0  # whether you want to transform y variable to ranks
+    rankx = 0  # whether you want to transform x variables to ranks
+    standardized = 1  # whether you want to standardize data
 
-    rank_corr, return_corr = showcorrs(pred_y, test_y)
-    saved_corr.append(rank_corr)
+    test_period = 24  # the number of peridos for testing
+    lag_period = 3  # the number of periods used in the training
 
-from statistics import mean
+    saved_corr = []
 
-print('mean_corr=', mean(saved_corr))
+    for t in range(T - test_period, T):
+        train_data = yxdata(t - 1, validdate, df, ranky, rankx)
+        for lagt in range(2, lag_period):
+            temp_train_data = yxdata(t - lagt, validdate, df, ranky, rankx)
+            train_data = pd.concat([train_data, temp_train_data])
 
-plt.plot(validdate[T - test_period - 1:T - 1], saved_corr, 'b-.+')
-plt.grid(True)
-plt.xticks(rotation=90)
-plt.show()
+        test_data = yxdata(t, validdate, df, ranky, rankx)
+
+        train_y = train_data['yreturns'].values.reshape(-1, 1)
+        test_y = test_data['yreturns'].values.reshape(-1, 1)
+
+        train_x = train_data[x_variable_names].values
+        test_x = test_data[x_variable_names].values
+
+        if standardized == 1:
+            scalar = StandardScaler().fit(train_x)
+            train_x = scalar.transform(train_x)
+            test_x = scalar.transform(test_x)
+
+        method = xgb.XGBRegressor(objective='reg:linear', colsample_bytree=0.25, learning_rate=0.002, max_depth=5,
+                                  alpha=50, n_estimators=5000, random_state=0)
+        method.fit(train_x, train_y)
+        pred_y = method.predict(test_x)
+        pred_y = pred_y.reshape(-1, 1)
+
+        rank_corr, return_corr = show_corrs(pred_y, test_y)
+        saved_corr.append(rank_corr)
+
+    print('mean_corr=', mean(saved_corr))
+
+    plt.plot(validdate[T - test_period - 1:T - 1], saved_corr, 'b-.+')
+    plt.grid(True)
+    plt.xticks(rotation=90)
+    plt.show()
