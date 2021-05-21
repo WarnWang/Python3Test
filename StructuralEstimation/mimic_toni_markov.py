043a62b9ae799937d00404d1cba261d692919423
Youan Wang <wangyouan0629@hotmail.com>
2021-05-21 16:40:53 +0800

    update directory structure

--- StructuralEstimation/mimic_toni_markov.py
+++ StructuralEstimation/mimic_toni_markov.py
@@ -7,18 +7,19 @@
 # @Email: markwang@connect.hku.hk
 
 import numpy as np
+from scipy.special import erf
 
 if __name__ == '__main__':
-    rho = 0.5
-    mu = 0.1421
+    rho = 0.8349
+    mu = -2.2067
     m = 2
     nz = 15
     f1 = np.zeros((nz, nz))
     f2 = np.ones((nz, nz))
     z = np.zeros(nz)
 
-    s2 = 0.0081
-    sig_z = np.sqrt(s2)
+    sig_z = 0.3594
+    s2 = sig_z ** 2
     sy = np.sqrt(s2 / (1 - rho ** 2))
     for i in range(nz):
         z[i] = -m * sy + ((2 * m * sy) / (nz - 1) * i)
@@ -27,8 +28,8 @@ if __name__ == '__main__':
     for j in range(nz):
         for i in range(1, nz):
             minif = (z[i] - rho * z[j] - w / 2) / sig_z
-            f1[i, j] = 0.5 * (1 + minif / np.sqrt(2))
-            f2[i - 1, j] = 0.5 * (1 + minif / np.sqrt(2))
+            f1[i, j] = 0.5 * (1 + erf(minif / np.sqrt(2)))
+            f2[i - 1, j] = 0.5 * (1 + erf(minif / np.sqrt(2)))
 
     z += mu
     f1 = f1.T
